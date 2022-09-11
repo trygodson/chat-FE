a039eba26dacd512e5f10fb483caf5a39ae8cd89
trygodson <gnqnpn@gmail.com>
2022-09-12 00:21:43 +0100

    something

--- src/shared/utils/api.ts
+++ src/shared/utils/api.ts
@@ -10,7 +10,8 @@ const headers: AxiosRequestHeaders = {
   'Content-Type': 'application/json',
   Authorization: item ? `Bearer ${item?.token}` : '',
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+  'Access-Control-Allow-Headers':
+    'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
   'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
 };
 
