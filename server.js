4e4b36960bd1c344aa4b31ef8453454468c2070e
trygodson <gnqnpn@gmail.com>
2022-09-11 22:17:58 +0100

    something

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static('build'));
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
